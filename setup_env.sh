cd27086c188b83f9644a9c26c29c4c450878a456
ChaunceyHon <144048187+ChaunceyHon@users.noreply.github.com>
2025-01-15 12:47:43 -0800

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -21,6 +21,6 @@ huggingface-cli download chunyu-li/LatentSync --local-dir checkpoints --exclude 
 
 # Soft links for the auxiliary models
 mkdir -p ~/.cache/torch/hub/checkpoints
-ln -s $(pwd)/checkpoints/auxiliary/2DFAN4-cd938726ad.zip ~/.cache/torch/hub/checkpoints/2DFAN4-cd938726ad.zip
-ln -s $(pwd)/checkpoints/auxiliary/s3fd-619a316812.pth ~/.cache/torch/hub/checkpoints/s3fd-619a316812.pth
-ln -s $(pwd)/checkpoints/auxiliary/vgg16-397923af.pth ~/.cache/torch/hub/checkpoints/vgg16-397923af.pth
+ln -sf $(pwd)/checkpoints/auxiliary/2DFAN4-cd938726ad.zip ~/.cache/torch/hub/checkpoints/2DFAN4-cd938726ad.zip
+ln -sf $(pwd)/checkpoints/auxiliary/s3fd-619a316812.pth ~/.cache/torch/hub/checkpoints/s3fd-619a316812.pth
+ln -sf $(pwd)/checkpoints/auxiliary/vgg16-397923af.pth ~/.cache/torch/hub/checkpoints/vgg16-397923af.pth

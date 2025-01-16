77b78d760da5a97956ef6202378646ab52243a74
ChaunceyHon <144048187+ChaunceyHon@users.noreply.github.com>
2025-01-15 16:13:10 -0800

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -25,7 +25,4 @@ ln -sf $(pwd)/checkpoints/auxiliary/2DFAN4-cd938726ad.zip ~/.cache/torch/hub/che
 ln -sf $(pwd)/checkpoints/auxiliary/s3fd-619a316812.pth ~/.cache/torch/hub/checkpoints/s3fd-619a316812.pth
 ln -sf $(pwd)/checkpoints/auxiliary/vgg16-397923af.pth ~/.cache/torch/hub/checkpoints/vgg16-397923af.pth
 
-#解决公共链接问题：
-wget https://cdn-media.hf-mirror.com/frpc-gradio-0.3/frpc_linux_amd64 -O frpc_linux_amd64_v0.3
-mv frpc_linux_amd64_v0.3 /root/miniconda3/envs/latentsync/lib/python3.10/site-packages/gradio/
-chmod +x /root/miniconda3/envs/latentsync/lib/python3.10/site-packages/gradio/frpc_linux_amd64_v0.3
+
